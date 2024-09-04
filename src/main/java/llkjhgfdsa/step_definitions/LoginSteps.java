ecf0a2fb3ad1f7b47bb6d55322c387145f60b4dd
attis82 <farkasatesz.82@gmail.com>
2024-09-04 11:58:04 +0200

    Resolves conflicts

--- src/main/java/llkjhgfdsa/step_definitions/LoginSteps.java
+++ src/main/java/llkjhgfdsa/step_definitions/LoginSteps.java
@@ -4,13 +4,14 @@ import io.cucumber.java.AfterAll;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import llkjhgfdsa.DriverContainer;
+import llkjhgfdsa.URL;
 import llkjhgfdsa.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 
 public class LoginSteps {
     @When("We click signup button")
     public void clickSignupButton() {
-        DriverContainer.createLoginPage().goToSignUp();
+        DriverContainer.get(URL.SIGNUP);
     }
 
     @AfterAll
